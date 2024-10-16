8a6c66a6d327cff73d083ad35cfe2af563c06d1e
valelectronic <nnanwikevictor@gmail.com>
2024-10-16 03:14:49 +0100

    debugging the backend functionality and completing the profile functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -154,8 +154,10 @@ export const refreshToken = async (req, res)=>{
 
 export const getProfile = async(req, res) =>{
     try {
+        res.json(req.user)
         
     } catch (error) {
+        res.status(500).json({message:"server error", error: error.message})
         
     }
 }
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 
 import { getProfile, login,logout,refreshToken,signup } from "../controllers/auth.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router()
 
@@ -8,6 +9,6 @@ router.post("/signup",signup )
 router.post("/login", login)
 router.post("/logout",logout)
 router.post("/refresh-token",refreshToken)
-router.get("/profile",getProfile)
+router.get("/profile", protectRoute, getProfile)
 
 export default router
