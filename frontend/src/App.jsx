d3b7ca6392c33d12a0b2c6a9cb0e848036589af2
valelectronic <nnanwikevictor@gmail.com>
2024-10-19 03:44:14 +0100

    installing the frontend packages, creating and setting up the navigation routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-
+import {Routes, Route} from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LogInPage from "./pages/LogInPage"
+import ProfilePage from "./pages/ProfilePage"
+import Navbar from "./components/Navbar"
 
 function App() {
- 
-
   return (
-    <>
-     <div>
-      <h1 className="text-red-500 text-5xl">hello </h1>
-     </div>
-    </>
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden">
+    <Navbar/>
+    <Routes>
+    < Route path="/" element = {<HomePage/>}/>
+    < Route path="/signUpPage" element = {<SignUpPage/>}/>
+    < Route path="/loginPage" element = {<LogInPage/>}/>
+    < Route path="/profilePage" element = {<ProfilePage/>}/>
+
+    </Routes>
+    </div>
   )
 }
 
